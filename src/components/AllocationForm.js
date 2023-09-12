00336a26e3ab3271dc4a12a2da1a0794d4eaf24f
BeBono <gitbebono@gmail.com>
2023-09-12 16:00:38 -0500

    Task-2 100%

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -46,9 +46,9 @@ const AllocationForm = (props) => {
                         <option value="Marketing" name="marketing"> Marketing</option>
                 <option value="Sales" name="sales">Sales</option>
                 <option value="Finance" name="finance">Finance</option>
-                <option value="HR" name="hr">HR</option>
+                <option value="Human Resource" name="hr">HR</option>
                 <option value="IT" name="it">IT</option>
-                <option value="Admin" name="admin">Admin</option>
+                {/* <option value="Admin" name="admin">Admin</option> */}
                   </select>
 
                     <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
