5d209604750d91b9a67811bcba70b85969b0bfb1
BeBono <gitbebono@gmail.com>
2023-09-15 10:52:05 -0500

    Inprove Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -21,18 +21,30 @@ import "../styles/style-currency.css";
 
   return (
     
-    <div className="alert alert-secondary currency"> 
-        <div className="mycustom-select">
-        {/* <span>Currency</span> */}
-      <select value={currency} onChange={(event) => setCurrency(event.target.value)} className="select-style" >
-        <option value="$"  className="option-style">Currency $ Dollar</option>
-        <option value="£" className="option-style" >Currency £ Pound</option>  {/* currency' hace que sea selecionado como valor inicial al arrancar la interfaz, ya que es el que está cargado como base. */}'
-        <option value="€" className="option-style" >Currency € Euro</option>
-        <option value="₹" className="option-style" >Currency ₹ Ruppee</option>
-      </select>
-    </div>
+    // <div className="alert alert-secondary currency"> 
+    //     <div className="mycustom-select">
+  
+    //   <select value={currency} onChange={(event) => setCurrency(event.target.value)} className="select-style" >
+    //     <option value="$"  className="option-style">Currency $ Dollar</option>
+    //     <option value="£" className="option-style" >Currency £ Pound</option>  {/* currency' hace que sea selecionado como valor inicial al arrancar la interfaz, ya que es el que está cargado como base. */}'
+    //     <option value="€" className="option-style" >Currency € Euro</option>
+    //     <option value="₹" className="option-style" >Currency ₹ Ruppee</option>
+    //   </select>
+    // </div>
 
-    </div>
+    // </div>
+
+    <div className='alert alert-secondary currency' >
+    <label style={{marginLeft: '2.4rem' , backgroundColor:'rgb(121, 199, 121)', color:'white', fontSize: '17px' }}> Currency
+    <select value={currency} name='hover_color'id="currency" onChange={(event)=>setCurrency(event.target.value)} style={{ marginLeft: '1rem' , backgroundColor:'rgb(121, 199, 121)', color:'white', border: 'none'}}>
+    <option style={{color:'black'}} value="$">Dollar ($)</option>
+    <option style={{color:'black'}}  value="£">Pound (£)</option>
+   
+    <option style={{color:'black'}} value="€">Euro (€)</option>
+    <option style={{color:'black'}}  value="₹">Ruppee (₹)</option>
+  </select>
+  </label>
+</div>
 
   );
 }
